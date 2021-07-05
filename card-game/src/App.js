edb25f7dcf25eae0a07325ca15a7de0d38963803
uekiz <kingghostdragon@gmail.com>
2021-07-05 11:03:49 +0700

    charactercard

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CharacterCard from './CharacterCard';
 import './App.css';
 function App() {
- return (
- <div>
- Hello World
- </div>
- );
+  return (
+    <div>
+    <CharacterCard value="h"/>
+    <CharacterCard value="i"/>
+    </div>
+    );
 }
 export default App;
